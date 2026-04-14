063f7979647203b66e11615263c4810c1d31ad19
ahmedMahfouz40 <ahmedmhfouz440@gmail.com>
2026-04-14 03:37:02 +0200

    second commit

--- src/proxy.ts
+++ src/proxy.ts
@@ -2,12 +2,12 @@ import { getToken } from "next-auth/jwt";
 import { NextRequest, NextResponse } from "next/server";
 
 export default async function proxy(req: NextRequest) {
-  const jwt = await getToken({ req });
+  const jwt = await getToken({ req , secret:process.env.NEXTAUTH_SECRET});
   // console.log("jwt from proxy ", jwt);
   if (jwt) {
     return NextResponse.next();
   }
-  return NextResponse.redirect(new URL("/login", process.env.NEXTAUTH_URL));
+  return NextResponse.redirect(new URL("/login", req.url));
 }
 export const config = {
   matcher: [ "/payment"  , "/allorders" , "/cart" ],
